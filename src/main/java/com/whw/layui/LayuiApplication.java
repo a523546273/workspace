d674b527e5f19b55a32390334ebaf31e4a8c81e7
wanghongwei <1084872667@qq.com>
2019-03-29 13:48:20 +0800

    添加redis

--- src/main/java/com/whw/layui/LayuiApplication.java
+++ src/main/java/com/whw/layui/LayuiApplication.java
@@ -2,8 +2,10 @@ package com.whw.layui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class LayuiApplication {
 
     public static void main(String[] args) {
